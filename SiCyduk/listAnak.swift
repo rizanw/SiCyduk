df9fe38d7dfa9b8d04746635714c9d852f95d3fc
rizanw <32553461+rizanw@users.noreply.github.com>
2019-04-04 11:58:08 +0700

    backup

---
+++ SiCyduk/listAnak.swift
@@ -0,0 +1,20 @@
+//
+//  listAnak.swift
+//  SiCyduk
+//
+//  Created by Rizky Andre on 10/9/17.
+//  Copyright © 2017 tc17. All rights reserved.
+//
+
+
+class listAnak {
+    var panggilan: String?
+    var idline: String?
+    
+    init(panggilan: String?, idline: String?) {
+        self.panggilan = panggilan
+        self.idline = idline
+    }
+    
+    
+}
